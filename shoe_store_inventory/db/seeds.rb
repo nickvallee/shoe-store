5337b81c7a6c91ed51db3cc743f3e539696329bc
Nick Vallee <nickvallee@gmail.com>
2023-11-24 10:46:13 -0500

    seed db

--- shoe_store_inventory/db/seeds.rb
+++ shoe_store_inventory/db/seeds.rb
@@ -2,8 +2,16 @@
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+STORE_NAMES = ['ALDO Centre Eaton', 'ALDO Destiny USA Mall', 'ALDO Pheasant Lane Mall', 'ALDO Holyoke Mall', 'ALDO Maine Mall', 'ALDO Crossgates Mall', 'ALDO Burlington Mall', 'ALDO Solomon Pond Mall', 'ALDO Auburn Mall', 'ALDO Waterloo Premium Outlets']
+SHOE_MODELS = ['ADERI', 'MIRIRA', 'CAELAN', 'BUTAUD', 'SCHOOLER', 'SODANO', 'MCTYRE', 'CADAUDIA', 'RASIEN', 'WUMA', 'GRELIDIEN', 'CADEVEN', 'SEVIDE', 'ELOILLAN', 'BEODA', 'VENDOGNUS', 'ABOEN', 'ALALIWEN', 'GREG', 'BOZZA']
+
+STORE_NAMES.each do |name|
+  Store.create(name: name)
+end
+
+Store.all.each do |store|
+  SHOE_MODELS.each do |model|
+    store.shoe_models.create(name: model, inventory: 0)
+  end
+end
